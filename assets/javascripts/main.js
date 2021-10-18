4665897960148aae7f4c27594533e78ca2f66934
johnhdez105 <johnhdez105@gmail.com>
2021-10-18 18:25:12 -0500

    Updated function to create a submit button element next to the input element

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,5 +1,9 @@
 function emFunc() {
   var x = document.createElement("INPUT");
+  var y = document.createElement("BUTTON");
+  var z = document.createTextNode("Submit");
   document.body.appendChild(x);
+  y.appendChild(z);
+  document.body.appendChild(y);
   document.getElementById("ema").innerHTML = "Enter email address below: ";
 }
