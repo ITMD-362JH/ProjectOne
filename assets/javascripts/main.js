1904d5607337fa7155acfd0d91b4d6689d353834
johnhdez105 <johnhdez105@gmail.com>
2021-10-18 18:26:39 -0500

    Created similar function for clicking phone radio button element

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -7,3 +7,13 @@ function emFunc() {
   document.body.appendChild(y);
   document.getElementById("ema").innerHTML = "Enter email address below: ";
 }
+
+function phFunc() {
+  var x = document.createElement("INPUT");
+  var y = document.createElement("BUTTON");
+  var z = document.createTextNode("Submit");
+  document.body.appendChild(x);
+  y.appendChild(z);
+  document.body.appendChild(y);
+  document.getElementById("ph").innerHTML = "Enter phone number below: ";
+}
